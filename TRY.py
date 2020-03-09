6442ebe61cc75bf603023f1ca154e89cdf942c06
bglcsl <61681208+bglcsl@users.noreply.github.com>
2020-03-09 18:15:57 +0800

    Update TRY.py

--- TRY.py
+++ TRY.py
@@ -1,41 +1,99 @@
 import sys
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QMessageBox
-from PyQt5.QtGui import QIcon, QImage, QPalette, QBrush
+from PyQt5.QtGui import QIcon, QImage, QPalette, QBrush, QPixmap, QFont
 from PyQt5.QtCore import pyqtSlot, QSize
 
 class Window3(QMainWindow):
     def __init__(self):
         super().__init__()
+        oImage = QImage("calculate.jpg")
+        self.setWindowIcon(QIcon('young-lion_97429.ico'))
+        sImage = oImage.scaled(QSize(550,350))   
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))                        
+        self.setPalette(palette)
         self.setGeometry(200,200,600,350)
         self.setWindowTitle("1")
         self.textboxlbl = QLabel("1",self)
+        self.buttonback = QPushButton("Back",self)
+        self.buttonback.setToolTip("Go back to topics")
+        self.buttonback.setStyleSheet("background-color:lightblue")
+        self.buttonback.move(150,250)
+        self.buttonback.clicked.connect(self.Window2)
+        
+    def Window2(self):
+        self.w = Window2()
+        self.w.show()
+        self.hide()
+
 
 class Window4(QMainWindow):
      def __init__(self):
         super().__init__()
+        self.setWindowIcon(QIcon('young-lion_97429.ico'))
+        oImage = QImage("calculate.jpg")
+        sImage = oImage.scaled(QSize(550,350))   
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))                        
+        self.setPalette(palette)
         self.setGeometry(200,200,600,350)
         self.setWindowTitle("2")
         self.textboxlbl = QLabel("2",self)
+        self.buttonback = QPushButton("Back",self)
+        self.buttonback.setToolTip("Go back to topics")
+        self.buttonback.setStyleSheet("background-color:lightblue")
+        self.buttonback.move(150,250)
+        self.buttonback.clicked.connect(self.Window2)
+        
+     def Window2(self):
+        self.w = Window2()
+        self.w.show()
+        self.hide()
     
 class Window5(QMainWindow):
     def __init__(self):
-        self.setGeometry(200,200,600,350)
         super().__init__()
+        self.setWindowIcon(QIcon('young-lion_97429.ico'))
+        self.setGeometry(200,200,600,350)
+        oImage = QImage("calculate.jpg")
+        sImage = oImage.scaled(QSize(550,350))   
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))                        
+        self.setPalette(palette)
         self.setWindowTitle("3")
         self.textboxlbl = QLabel("3",self)
+        self.buttonback = QPushButton("Back",self)
+        self.buttonback.setToolTip("Go back to topics")
+        self.buttonback.setStyleSheet("background-color:lightblue")
+        self.buttonback.move(150,250)
+        self.buttonback.clicked.connect(self.Window2)
+        
+    def Window2(self):
+        self.w = Window2()
+        self.w.show()
+        self.hide()
     
     
 class Window2(QMainWindow):                         
     def __init__(self):
         super().__init__()
+        
         self.setWindowTitle("PHYSICS(calculus-based)")
         self.setWindowIcon(QIcon('young-lion_97429.ico')) 
-
+        oImage = QImage("first.jpg")
+        sImage = oImage.scaled(QSize(550,350))   
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))                        
+        self.setPalette(palette)
+        self.setStyleSheet
         self.setGeometry(200,200,600,350)
         self.textboxlbl = QLabel("<h1>PHYSICS</h1>",self)
-        self.textboxlbl.move(245, 10)
+        self.textboxlbl.move(255, 30)
+        self.textboxlbl.setFont(QtGui.QFont('Lucida Handwriting', 7))
         self.textboxlb2 = QLabel("<h2>Topics</h2>",self)
-        self.textboxlb2.move(265, 35)
+        self.textboxlb2.move(265, 55)
+        self.textboxlb2.setFont(QtGui.QFont('Castellar', 7))
         self.button2 = QPushButton("CALCULATE", self)
         self.button2.setToolTip("Calculate the formula")
         self.button2.setStyleSheet("background-color:pink")
@@ -51,11 +109,11 @@ class Window2(QMainWindow):
         self.button4.setStyleSheet("background-color:pink")
         self.button4.move(150,210)
         self.button4.resize(300,30)
+        
 
         self.button2.clicked.connect(self.Window3)
         self.button3.clicked.connect(self.Window4)
         self.button4.clicked.connect(self.Window5)
-
 
 
     def Window3(self):                                             # <===
@@ -72,7 +130,9 @@ class Window2(QMainWindow):
         self.w = Window5()
         self.w.show()
         self.hide()
-
+    
+    
+    
 
 class App(QWidget):
 
@@ -98,13 +158,15 @@ class App(QWidget):
         self.setPalette(palette)
         
         self.textboxlbl = QLabel("<h1>PHYSICS</h1>",self)
-        self.textboxlbl.move(245, 130)
+        self.textboxlbl.move(200, 70)
+        self.textboxlbl.setFont(QtGui.QFont('Lucida Fax',18))
         self.textboxlb2= QLabel("<h2>Calculus-Based</h2>",self)
-        self.textboxlb2.move(225, 160)
+        self.textboxlb2.move(215, 130)
+        self.textboxlb2.setFont(QtGui.QFont('Monotype Corsiva', 13))
 
         self.button = QPushButton('EXPLORE', self)
         self.button.setToolTip("Explore the wonderinfo")
-        self.button.move(255,190)
+        self.button.move(255,170)
         self.button.setStyleSheet("background-color:lightblue")
        
         self.button.clicked.connect(self.Window2)
